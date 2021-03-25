93d6ccbe5689037212cd3b1a3d5f48f2d30423b5
L S <61838582+leon-bot-max@users.noreply.github.com>
2021-03-25 13:23:55 +0100

    program

--- Felhantering-Uppgift/Program.cs
+++ Felhantering-Uppgift/Program.cs
@@ -10,6 +10,30 @@ namespace Felhantering_Uppgift
     {
         static void Main(string[] args)
         {
+
+            
+
+
+            Console.Write("Mata in start ");
+            string startInput = Console.ReadLine();
+            Console.Write("Mata in stop ");
+            string stopInput = Console.ReadLine();
+            Console.Write("Mata in steg ");
+            string stepInput = Console.ReadLine();
+
+            if (int.TryParse(startInput, out int start) && int.TryParse(stopInput, out int stop) && int.TryParse(stepInput, out int step))
+            {
+
+                for (int i = start; i <= stop; i+= step)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
